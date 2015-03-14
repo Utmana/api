32e5fb8646eb3a3de7db5bf2c8f8fc5f54eef860
irony <cl@iteam.se>
2015-03-14 17:59:07 +0100

    register cors before routes

--- api/index.js
+++ api/index.js
@@ -4,13 +4,14 @@ var express = require('express');
 var routes = require('./routes');
 var cors = require('express-cors');
 var app = express();
-routes.register(app);
 
 app.use(cors({
   allowedOrigins: [
     '*'
   ]
 }));
+
+routes.register(app);
 
 app.listen(process.env.PORT || 3000, console.log);
 
