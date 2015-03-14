a269683bacda4ece314af3ef39f805cfa6c8caf6
irony <cl@iteam.se>
2015-03-14 16:59:41 +0100

    added cors

--- api/index.js
+++ api/index.js
@@ -2,9 +2,15 @@
 
 var express = require('express');
 var routes = require('./routes');
+var cors = require('express-cors');
 var app = express();
-
 routes.register(app);
+
+app.use(cors({
+  allowedOrigins: [
+    '*'
+  ]
+}));
 
 app.listen(process.env.PORT || 3000, console.log);
 
